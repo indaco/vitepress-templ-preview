fd2c6e232464a282d7d1998f052a8bef865c534d
indaco <github@mircoveltri.me>
2024-07-10 18:19:39 +0200

    chore: rename templ file for button example as button-demo.templ

--- website/docs/templ-preview/demos/button/button-demo_templ.go
+++ website/docs/templ-preview/demos/button/button-demo_templ.go
@@ -87,7 +87,7 @@ func button(label string) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(label)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `demos/button/button-primary.templ`, Line: 19, Col: 64}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `demos/button/button-demo.templ`, Line: 19, Col: 64}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
