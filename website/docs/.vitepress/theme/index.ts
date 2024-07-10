8044c5ec4005703784510a10aa402432329cf664
indaco <github@mircoveltri.me>
2024-07-10 18:22:33 +0200

    refactor: package exports

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from "vitepress/theme";
-import { VTPIconTabs } from "vitepress-templ-preview/ui";
-import "vitepress-templ-preview/ui/style.css";
+import { VTPIconTabs } from "vitepress-templ-preview/components";
+import "vitepress-templ-preview/style.css";
 
 export default {
   ...DefaultTheme,
