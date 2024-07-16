438b9af57d618ffd22f48d42e90f0cd3ab906a9a
indaco <github@mircoveltri.me>
2024-07-16 14:45:16 +0200

    chore: add highlighted variants to logger

--- package/src/logger.ts
+++ package/src/logger.ts
@@ -2,6 +2,7 @@ import ansis from 'ansis';
 
 export class Logger {
   static PREFIX = 'vitepress-templ-preview';
+
   static log(
     headline: string,
     message: string,
@@ -18,7 +19,11 @@ export class Logger {
   }
 
   static info(headline: string, message: string) {
-    this.log(headline, message, Logger.PREFIX, ansis.bold.blueBright);
+    this.log(headline, message, Logger.PREFIX, ansis.bold.blue);
+  }
+
+  static success(headline: string, message: string) {
+    this.log(headline, message, Logger.PREFIX, ansis.bold.green);
   }
 
   static error(headline: string, message: string) {
@@ -27,5 +32,42 @@ export class Logger {
 
   static warning(headline: string, message: string) {
     this.log(headline, message, Logger.PREFIX, ansis.bold.yellow);
+  }
+
+  static logHighlighted(
+    headline: string,
+    message: string,
+    bgColor: (text: string) => string,
+    textColor: (text: string) => string,
+  ) {
+    console.log(
+      `${bgColor(`[${Logger.PREFIX}]`)} ${textColor(`${headline}`)} ${ansis.gray(`${message}`)}`,
+    );
+  }
+
+  static infoHighlighted(headline: string, message: string) {
+    this.logHighlighted(headline, message, ansis.black.bgBlue, ansis.bold.blue);
+  }
+
+  static suceessHighlighted(headline: string, message: string) {
+    this.logHighlighted(
+      headline,
+      message,
+      ansis.black.bgGreen,
+      ansis.bold.green,
+    );
+  }
+
+  static errorHighlighted(headline: string, message: string) {
+    this.logHighlighted(headline, message, ansis.black.bgRed, ansis.bold.red);
+  }
+
+  static warningHighlighted(headline: string, message: string) {
+    this.logHighlighted(
+      headline,
+      message,
+      ansis.black.bgYellow,
+      ansis.bold.yellow,
+    );
   }
 }
