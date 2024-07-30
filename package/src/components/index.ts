0748943483c399afb79582c53ec5addc8236842a
indaco <github@mircoveltri.me>
2024-07-30 19:06:45 +0200

    chore(components): remove component props from export

--- package/src/components/index.ts
+++ package/src/components/index.ts
@@ -1,11 +1,4 @@
-export { default as VTPCard, type VTPCardProps } from './VTPCard.vue';
-export {
-  default as VTPCodeToggle,
-  type VTPCodeToggleProps,
-} from './VTPCodeToggle.vue';
-
-export { default as VTPTabs, type VTPTabsProps } from './VTPTabs.vue';
-export {
-  default as VTPIconTabs,
-  type VTPIconTabsProps,
-} from './VTPIconTabs.vue';
+export { default as VTPCard } from './VTPCard.vue';
+export { default as VTPCodeToggle } from './VTPCodeToggle.vue';
+export { default as VTPTabs } from './VTPTabs.vue';
+export { default as VTPIconTabs } from './VTPIconTabs.vue';
