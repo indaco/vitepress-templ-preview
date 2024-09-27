77777f1e3667547836cd6544f6d25eb66f379bb5
indaco <github@mircoveltri.me>
2024-09-27 08:28:09 +0200

    chore(website): remove redundant import for VPBadge component

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -1,6 +1,5 @@
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import { VPBadge } from 'vitepress/theme';
 import VersionBadge from '../../src/components/VersionBadge.vue';
 import { VTPIconTabs } from 'vitepress-templ-preview/components';
 import 'vitepress-templ-preview/style.css';
@@ -9,9 +8,8 @@ import 'virtual:group-icons.css';
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    app.component('Badge', VPBadge);
+    app.component('VersionBadge', VersionBadge);
     /* Make sure to name the tag  `VTPLivePreview` */
     app.component('VTPLivePreview', VTPIconTabs);
-    app.component('VersionBadge', VersionBadge);
   },
 } satisfies Theme;
