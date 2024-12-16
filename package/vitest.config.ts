91274eb1476ab95c1519e599a40eaf215c901630
Mirco Veltri <github@mircoveltri.me>
2024-12-16 17:10:21 +0100

    test(package): setup vitest coverage

--- package/vitest.config.ts
+++ package/vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      reporter: ['html'],
+      extension: ['.ts'],
+      exclude: ['./*.ts', 'src/components/**', 'src/plugin/**'],
+    },
   },
 });
