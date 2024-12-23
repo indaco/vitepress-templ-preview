abb51a19ce202c24013195a88fe0526a14795e80
Mirco Veltri <github@mircoveltri.me>
2024-12-23 16:12:09 +0100

    refactor(website): use explicit node prefix for fs and path modules

--- website/docs/src/plugins/injectVersion.ts
+++ website/docs/src/plugins/injectVersion.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 export function getVersionFromPackageJson(): string {
   const packageJsonPath = join(__dirname, '../../../../package/package.json');
